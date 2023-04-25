6660753de8afa3ceb56085f2f48b1773a21a9922
amir1381_barahimi <amirmohsenbarahimi1381@gmail.com>
2023-04-25 16:01:40 +0430

    edit initial models

--- YogaMana/Authentication/models.py
+++ YogaMana/Authentication/models.py
@@ -14,10 +14,20 @@ class Member(models.Model):
     last_name = models.CharField(max_length=200)
     user_name = models.CharField(unique=True,max_length=200)
     password = models.CharField(unique=True,max_length=200)
+    phone = models.CharField(max_length=11 , default='+98')
     email = models.EmailField(max_length=20,unique=True)
-    #membership = models.ForeignKey(models.Membershi, verbose_name=_(""), on_delete=models.CASCADE)
+    #membership = models.models.OneToOneField(Membership, verbose_name=_(""), on_delete=models.CASCADE)(models.Membership, verbose_name=_(""), on_delete=models.CASCADE)
     created_date = models.DateField()
-    #profile_image = models.ImageField()
+    profile_image = models.ImageField()
+    
+class Address(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    country = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    address = models.TextField(max_length=300)
+    
+
+    
 
 
     
