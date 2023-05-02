81ce80e6d1345a06144705f629ba96d8d4be4c70
amir1381_barahimi <amirmohsenbarahimi1381@gmail.com>
2023-05-02 21:19:55 +0330

    edit url name

--- YogaMana/Authentication/urls.py
+++ YogaMana/Authentication/urls.py
@@ -4,8 +4,8 @@ from . import views
 #app_name='Authentication'
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path("memberList/", views.MemberListgenerics.as_view() , name='members_list_info'),
+    path("member-list/", views.MemberListgenerics.as_view() , name='members_list_info'),
     path("member/<int:pk>/", views.MemberDetailGeneric.as_view() , name='member_detail_info'),
-    path("userList/", views.UserListgenerics.as_view() , name='user_list_info'),
+    path("user-list/", views.UserListgenerics.as_view() , name='user_list_info'),
     path("user/<int:pk>/", views.UserDetailGeneric.as_view() , name='users_detail_info'),
 ]
