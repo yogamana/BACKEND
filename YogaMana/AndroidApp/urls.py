8a70f2f1abba3fc3d92583b2873a65a2a75f2f02
amir1381_barahimi <amirmohsenbarahimi1381@gmail.com>
2023-06-02 21:18:46 +0330

    update url

--- YogaMana/AndroidApp/urls.py
+++ YogaMana/AndroidApp/urls.py
@@ -41,5 +41,4 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
